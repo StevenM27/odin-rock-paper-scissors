b3daf3fa3abf554e3d5bb5c0fd9b2d9652930614
Steven Martin <83900183+StevenM27@users.noreply.github.com>
2024-06-01 18:37:23 -0700

    Create playRound function

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
   const choiceID = Math.floor(Math.random() * 3);
   let choice;
@@ -28,8 +32,56 @@ function getHumanChoice() {
   return prompt("Rock, Paper, or Scissors?").toLowerCase();
 }
 
-const playerChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
 
-console.log(playerChoice);
-console.log(computerChoice);
+function win(humanChoice, computerChoice) {
+  console.log("You win! " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + " beats " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + ".");
+  humanScore++;
+}
+
+
+function lose(humanChoice, computerChoice) {
+  console.log("You lose! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + " beats " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + ".");
+  computerScore++;
+}
+
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Tie! You both chose " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1));
+    return;
+  }
+  
+  if (humanChoice === "rock") {
+    if (computerChoice === "paper") {
+      lose(humanChoice, computerChoice);
+    }
+    else {
+      win(humanChoice, computerChoice);
+    }
+  }
+  else if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      win(humanChoice, computerChoice);
+    }
+    else {
+      lose(humanChoice, computerChoice);
+    }
+  }
+  else {
+    if (computerChoice === "rock") {
+      lose(humanChoice, computerChoice);
+    }
+    else {
+      win(humanChoice, computerChoice);
+    }
+  }
+}
+
+
+const playerSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection);
+
+playRound(playerSelection, computerSelection);
